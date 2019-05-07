94566bf733acd92247c305e8f95270461f2afca6
fugang <fgcui1204@163.com>
2019-05-07 22:23:46 +0800

    add flag to reduce no order changed.

--- src/main/java/BubbleSorting.java
+++ src/main/java/BubbleSorting.java
@@ -3,12 +3,18 @@ class BubbleSorting {
     int[] bubble(int[] data) {
 
         for (int j = 0; j < data.length - 1; j++) {
+            boolean NoChangedSorting = true;
             for (int i = 0; i < data.length - 1 -j; i++) {
                 if (data[i] > data[i + 1]) {
                     int temp = data[i + 1];
                     data[i + 1] = data[i];
                     data[i] = temp;
+                    NoChangedSorting = false;
                 }
+
+            }
+            if (NoChangedSorting) {
+                break;
             }
         }
         
