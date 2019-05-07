2ba1a22dacff233388cb3fbfc3c66ebf9125d5d3
fugang <fgcui1204@163.com>
2019-05-07 22:19:06 +0800

    version 2 is ok. reduce already sorted.

--- src/main/java/BubbleSorting.java
+++ src/main/java/BubbleSorting.java
@@ -3,7 +3,7 @@ class BubbleSorting {
     int[] bubble(int[] data) {
 
         for (int j = 0; j < data.length - 1; j++) {
-            for (int i = 0; i < data.length - 1; i++) {
+            for (int i = 0; i < data.length - 1 -j; i++) {
                 if (data[i] > data[i + 1]) {
                     int temp = data[i + 1];
                     data[i + 1] = data[i];
